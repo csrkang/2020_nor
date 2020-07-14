d6a32558eaec9192261630599b29fece28de63bb
Christina Kang-Yun <csrkang@umich.edu>
2020-07-14 12:06:58 -0400

    gtf file download excluded from .sh

--- download_SRX.sh
+++ download_SRX.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 wget -P fasta/ https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/002/752/655/GCA_002752655.1_ASM275265v1/GCA_002752655.1_ASM275265v1_genomic.fna.gz
 gunzip fasta/GCA_002752655.1_ASM275265v1_genomic.fna.gz
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/002/752/655/GCA_002752655.1_ASM275265v1/GCA_002752655.1_ASM275265v1_genomic.gtf.gz
-gunzip GCA_002752655.1_ASM275265v1_genomic.gtf
 bwa index fasta/GCA_002752655.1_ASM275265v1_genomic.fna
 fileName='SRX_Acc_List.txt'
 while IFS= read -r line
